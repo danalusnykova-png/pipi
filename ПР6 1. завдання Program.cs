7c6303e91fa2d80c906700af5f2a415f0411663d
danalusnykova-png <danalusnykova@gmail.com>
2026-06-03 21:24:34 +0300

    Add files via upload

---
+++ ПР6 1. завдання Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+
+namespace Practice6_Rectangle
+{
+    class Rectangle
+    {
+        private double sideA;
+        private double sideB;
+
+        public Rectangle()
+        {
+            sideA = 1;
+            sideB = 1;
+        }
+
+        public Rectangle(double a, double b)
+        {
+            sideA = a;
+            sideB = b;
+        }
+
+        public void SetSides(double a, double b)
+        {
+            sideA = a;
+            sideB = b;
+        }
+
+        public double GetSideA()
+        {
+            return sideA;
+        }
+
+        public double GetSideB()
+        {
+            return sideB;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (sideA + sideB);
+        }
+
+        public double GetArea()
+        {
+            return sideA * sideB;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Сторона A: {sideA}");
+            Console.WriteLine($"Сторона B: {sideB}");
+            Console.WriteLine($"Периметр: {GetPerimeter()}");
+            Console.WriteLine($"Площа: {GetArea()}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Практична 6, Лусникова Даніелла";
+
+            Console.WriteLine("Автор проєкту: Лусникова Даніелла");
+            Console.WriteLine("Практична 6");
+            Console.WriteLine("Завдання 1. Клас Rectangle");
+            Console.WriteLine();
+
+            Rectangle rectangle = new Rectangle(5, 8);
+
+            Console.WriteLine("Інформація про один прямокутник:");
+            rectangle.PrintInfo();
+
+            Console.WriteLine();
+
+            Rectangle[] rectangles = new Rectangle[3];
+
+            rectangles[0] = new Rectangle(4, 6);
+            rectangles[1] = new Rectangle(7, 9);
+            rectangles[2] = new Rectangle(10, 12);
+
+            Console.WriteLine("Інформація про масив прямокутників:");
+
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Прямокутник №{i + 1}");
+                rectangles[i].PrintInfo();
+            }
+
+            double sumA = 0;
+            double sumB = 0;
+
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                sumA += rectangles[i].GetSideA();
+                sumB += rectangles[i].GetSideB();
+            }
+
+            double averageA = sumA / rectangles.Length;
+            double averageB = sumB / rectangles.Length;
+
+            Rectangle averageRectangle = new Rectangle(averageA, averageB);
+
+            Console.WriteLine();
+            Console.WriteLine("Прямокутник із середніми значеннями сторін:");
+            averageRectangle.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ПР6 2завдання Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+
+namespace Practice6_Person
+{
+    class Person
+    {
+        private string surname;
+        private double height;
+        private double weight;
+        private int birthYear;
+
+        public Person()
+        {
+            surname = "Невідомо";
+            height = 0;
+            weight = 0;
+            birthYear = 0;
+        }
+
+        public Person(string surname, double height, double weight, int birthYear)
+        {
+            this.surname = surname;
+            this.height = height;
+            this.weight = weight;
+            this.birthYear = birthYear;
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int BirthYear
+        {
+            get { return birthYear; }
+            set { birthYear = value; }
+        }
+
+        public int GetAge(int currentYear)
+        {
+            return currentYear - birthYear;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Прізвище: " + surname);
+            Console.WriteLine("Зріст: " + height + " см");
+            Console.WriteLine("Вага: " + weight + " кг");
+            Console.WriteLine("Рік народження: " + birthYear);
+            Console.WriteLine("Вік: " + GetAge(2026) + " років");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Практична робота №6. Завдання 2";
+
+            Console.WriteLine("Автор проєкту: Лусникова Даніелла");
+            Console.WriteLine("Практична робота №6");
+            Console.WriteLine("Завдання 2. Клас Person");
+            Console.WriteLine();
+
+            Person person1 = new Person("Лусникова", 167, 50, 2007);
+            Person person2 = new Person("Іваненко", 175, 68, 2006);
+            Person person3 = new Person("Петренко", 182, 76, 2005);
+
+            Console.WriteLine("Інформація про першу особу:");
+            person1.PrintInfo();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Інформація про другу особу:");
+            person2.PrintInfo();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Інформація про третю особу:");
+            person3.PrintInfo();
+
+            Console.WriteLine();
+
+            double averageWeight = (person1.Weight + person2.Weight + person3.Weight) / 3;
+
+            Console.WriteLine("Середня вага осіб: " + averageWeight.ToString("0.00") + " кг");
+
+            Console.ReadKey();
+        }
+    }
+}
