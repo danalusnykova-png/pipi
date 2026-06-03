a0945b692baaeb297b949ad2e0ecf2bc873df29e
danalusnykova-png <danalusnykova@gmail.com>
2026-06-03 22:05:54 +0300

    Add files via upload

---
+++ ПР7 1завдання Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+namespace Praktychna7_Task1
+{
+    class Stack
+    {
+        private int[] data;
+        private int top;
+
+        public Stack(int size)
+        {
+            data = new int[size];
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == data.Length - 1;
+        }
+
+        public void Push(int value)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Стек переповнений. Неможливо додати елемент.");
+            }
+            else
+            {
+                top++;
+                data[top] = value;
+                Console.WriteLine("Додано у стек: " + value);
+            }
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Стек порожній. Неможливо вилучити елемент.");
+                return 0;
+            }
+            else
+            {
+                int value = data[top];
+                top--;
+                return value;
+            }
+        }
+
+        public void PrintStack()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Стек порожній.");
+            }
+            else
+            {
+                Console.WriteLine("Елементи стеку:");
+
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(data[i]);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Практична 7. Завдання 1";
+
+            Console.WriteLine("Автор проєкту: Лусникова Даніелла");
+            Console.WriteLine("Практична 7");
+            Console.WriteLine("Завдання 1. Реалізація стеку для чисел");
+            Console.WriteLine();
+
+            Stack stack = new Stack(5);
+
+            stack.Push(10);
+            stack.Push(25);
+            stack.Push(40);
+
+            Console.WriteLine();
+            stack.PrintStack();
+
+            Console.WriteLine();
+            int deletedElement = stack.Pop();
+            Console.WriteLine("Вилучений елемент зі стеку: " + deletedElement);
+
+            Console.WriteLine();
+            stack.PrintStack();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ПР7 2завдання Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+namespace Praktychna7_Task2
+{
+    class WordStack
+    {
+        private string[] words;
+        private int top;
+
+        public WordStack(int size)
+        {
+            words = new string[size];
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == words.Length - 1;
+        }
+
+        public void Push(string word)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Стек переповнений. Неможливо додати слово.");
+            }
+            else
+            {
+                top++;
+                words[top] = word;
+                Console.WriteLine("Додано у стек слово: " + word);
+            }
+        }
+
+        public string Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Стек порожній. Неможливо вилучити слово.");
+                return "";
+            }
+            else
+            {
+                string word = words[top];
+                top--;
+                return word;
+            }
+        }
+
+        public void PrintStack()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Стек порожній.");
+            }
+            else
+            {
+                Console.WriteLine("Слова у стеку:");
+
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "Практична 7. Завдання 2";
+
+            Console.WriteLine("Автор проєкту: Лусникова Даніелла");
+            Console.WriteLine("Практична 7");
+            Console.WriteLine("Завдання 2. Реалізація стеку для слів");
+            Console.WriteLine();
+
+            WordStack stack = new WordStack(5);
+
+            stack.Push("Cybersecurity");
+            stack.Push("Data");
+            stack.Push("Network");
+
+            Console.WriteLine();
+            stack.PrintStack();
+
+            Console.WriteLine();
+            string deletedWord = stack.Pop();
+            Console.WriteLine("Вилучене слово зі стеку: " + deletedWord);
+
+            Console.WriteLine();
+            stack.PrintStack();
+
+            Console.ReadKey();
+        }
+    }
+}
